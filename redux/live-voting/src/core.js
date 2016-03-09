cb02ccccd210bd9abaf48e6f93bcc9d27f4c8202
Manassarn Manoonchai <manassarn@gmail.com>
2016-03-10 02:19:14 +0700

    Add next()

--- redux/live-voting/src/core.js
+++ redux/live-voting/src/core.js
@@ -3,3 +3,14 @@ import { List } from 'immutable';
 export function setEntries (state, entries) {
   return state.set('entries', List(entries));
 }
+
+export function next (state) {
+  const entries = state.get('entries');
+
+  return state.merge({
+    vote: {
+      pair: entries.take(2)
+    },
+    entries: entries.skip(2)
+  })
+}
--- redux/live-voting/test/core_spec.js
+++ redux/live-voting/test/core_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
 
-import { setEntries } from '../src/core';
+import { setEntries, next } from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -12,6 +12,22 @@ describe('application logic', () => {
 
       expect(nextState).to.equal(Map({
         entries: List.of('Waterzonic', 'S2O Festival')
+      }));
+    });
+  });
+
+  describe('next', () => {
+    it('takes next 2 entries under vode', () => {
+      const state = Map({
+        entries: List.of('Waterzonic', 'S2O Festival', 'Take That')
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Waterzonic', 'S2O Festival')
+        }),
+        entries: List.of('Take That')
       }));
     });
   });
