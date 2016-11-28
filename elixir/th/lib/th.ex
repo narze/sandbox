d56e68e3c640a519380dac59163acfa4f2b38caf
Manassarn Manoonchai <manassarn@gmail.com>
2016-11-29 03:13:04 +0700

    Count frequency thai characters

--- elixir/th/lib/th.ex
+++ elixir/th/lib/th.ex
@@ -1,2 +1,21 @@
 defmodule TH do
+  def count_chars(string) do
+    Regex.replace(~r/[^\w\dก-๛-]/u, string, "")
+    |> String.codepoints
+    |> Enum.reduce(%{}, fn(char, result) ->
+      Map.update(result, char, 1, &(&1 + 1))
+    end)
+  end
+
+  def sort(map) do
+    Enum.sort map, fn({_,num1}, {_,num2}) ->
+      num1 > num2
+    end
+  end
+
+  def count_in_file(filename) do
+    File.read!(filename)
+    |> count_chars
+    |> sort
+  end
 end
--- elixir/th/test/th_test.exs
+++ elixir/th/test/th_test.exs
@@ -2,7 +2,34 @@ defmodule THTest do
   use ExUnit.Case
   doctest TH
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "count characters from words" do
+    assert TH.count_chars("word") == %{ "w" => 1, "o" => 1, "r" => 1, "d" => 1 }
+  end
+
+  test "excepts newline & space characters" do
+    assert TH.count_chars("wo rd\n") == %{ "w" => 1, "o" => 1, "r" => 1, "d" => 1 }
+  end
+
+  test "supports thai characters" do
+    assert TH.count_chars("สวัสดี") == %{"ส" => 2, "ว" => 1, "ั" => 1, "ด" => 1, "ี" => 1 }
+  end
+
+  test "sort by count" do
+    map = %{"ด" => 1, "ท" => 1, "ภ" => 1, "ย" => 4, "ว" => 1, "ษ" => 1,
+  "ส" => 2, "ั" => 1, "า" => 2, "ี" => 1, "ไ" => 1}
+    expected = [{"ย", 4}, {"า", 2}, {"ส", 2}, {"ไ", 1}, {"ี", 1}, {"ั", 1},
+ {"ษ", 1}, {"ว", 1}, {"ภ", 1}, {"ท", 1}, {"ด", 1}]
+
+    assert TH.sort(map) == expected
+  end
+
+  test "count in file" do
+    assert TH.count_in_file('test/words.txt') == [
+      {"ส", 2},
+      {"ี", 1},
+      {"ั", 1},
+      {"ว", 1},
+      {"ด", 1},
+    ]
   end
 end
